33651ae010d57b32f83f0bcd973a3c8a9c83af32
tbray <tbray@ccmbenchmark.com>
2024-02-05 10:39:24 +0100

    feat(Normalizer): Add brick/geo to suggest, and add test to handle it

--- src/Serializer/Normalizer/GeometryNormalizer.php
+++ src/Serializer/Normalizer/GeometryNormalizer.php
@@ -26,11 +26,15 @@ use function json_encode;
 final class GeometryNormalizer implements DenormalizerInterface, CacheableSupportsMethodInterface, NormalizerInterface
 {
     /**
-     * @param  $data
+     * @param mixed $data
      * @param array{deserialization_path?: null|string} $context
      */
     public function denormalize(mixed $data, string $type, ?string $format = null, array $context = []): Geometry
     {
+        if (!class_exists(Geometry::class) || !class_exists(GeoJSONReader::class)) {
+            throw new \RuntimeException("Package brick/geo is required to handle Geometry. Please run `composer require brick/geo`");
+        }
+
         if (!is_array($data)) {
             throw NotNormalizableValueException::createForUnexpectedDataType(
                 'The data should be a geojson string.',
@@ -47,7 +51,18 @@ final class GeometryNormalizer implements DenormalizerInterface, CacheableSuppor
                 throw new Exception("Cannot encode to json to array");
             }
 
-            return (new GeoJSONReader())->read($geometryString);
+            $denormalize = (new GeoJSONReader())->read($geometryString);
+
+            if (!$denormalize instanceof Geometry) {
+                throw NotNormalizableValueException::createForUnexpectedDataType(
+                    'The data should be a Geometry geojson string. Feature and FeatureCollection are not supported',
+                    $data,
+                    [Type::BUILTIN_TYPE_ARRAY],
+                    $context['deserialization_path'] ?? null,
+                    true,
+                );
+            }
+            return $denormalize;
 
         } catch (Exception $e) {
             throw NotNormalizableValueException::createForUnexpectedDataType(
@@ -64,7 +79,7 @@ final class GeometryNormalizer implements DenormalizerInterface, CacheableSuppor
 
     public function supportsDenormalization(mixed $data, string $type, ?string $format = null): bool
     {
-        return $type === Geometry::class || is_subclass_of($type, Geometry::class);
+        return class_exists(Geometry::class) && ($type === Geometry::class || is_subclass_of($type, Geometry::class));
     }
 
     public function hasCacheableSupportsMethod(): bool
@@ -78,6 +93,10 @@ final class GeometryNormalizer implements DenormalizerInterface, CacheableSuppor
      */
     public function normalize(mixed $object, ?string $format = null, array $context = [])
     {
+        if (!class_exists(Geometry::class) || !class_exists(GeoJSONWriter::class)) {
+            throw new \RuntimeException("Package brick/geo is required to handle Geometry. Please run `composer require brick/geo`");
+        }
+
         if (!$object instanceof Geometry) {
             throw NotNormalizableValueException::createForUnexpectedDataType(
                 'The data should be a Geometry Object.',
@@ -92,6 +111,6 @@ final class GeometryNormalizer implements DenormalizerInterface, CacheableSuppor
 
     public function supportsNormalization(mixed $data, ?string $format = null): bool
     {
-        return $data instanceof Geometry;
+        return class_exists(Geometry::class) && $data instanceof Geometry;
     }
 }
